1154dffa3aa8a714d183d9048fe9381dfdb0a28d
Josh Gillies <github@joshgilli.es>
2015-02-01 18:24:20 +1100

    Add project settings via rc

--- app/server.js
+++ app/server.js
@@ -1,10 +1,12 @@
 var jadeStream = require('jade-stream')
+var config = require('rc')('bc-webplayer');
 var http = require('http')
 var st = require('st')
 
 var mount = st({
   path: 'app/assets',
-  index: 'index.jade'
+  index: 'index.jade',
+  cache: config.cache
 })
 
 var app = http.createServer(function server(req, res) {
