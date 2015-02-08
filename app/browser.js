dd82e4f759a124a3c48a10d50883fcc0f162bf21
Josh Gillies <github@joshgilli.es>
2015-02-08 22:47:02 +1100

    Update track listing

--- app/browser.js
+++ app/browser.js
@@ -1,4 +1,5 @@
 var document = require('global/document');
+var cuid = require('cuid');
 var hg = require('mercury');
 var h = require('mercury').h;
 
@@ -6,9 +7,13 @@ var track = 'https://ia902707.us.archive.org/17/items/NodeUp81/NodeUp81.mp3';
 
 function App() {
   return hg.state({
-    track: hg.struct({
-      mp3: hg.value(track)
-    }),
+    tracks: hg.array([
+      hg.struct({
+        id: cuid(),
+        playing: hg.value(false),
+        mp3: hg.value(track)
+      })
+    ]),
     channels: {
       addTrack: addTrack
     }
@@ -16,18 +21,30 @@ function App() {
 }
 
 function addTrack(state, data) {
-  state.track.mp3.set(data.url);
+  state.tracks.push(
+    hg.struct({
+      id: cuid(),
+      playing: hg.value(false),
+      mp3: hg.value(data.url)
+    })
+  );
 }
 
 App.render = function render(state) {
   return h('div', [
     h('audio', {
+      'id': 'player',
       'controls': true
     }, [
       h('source', {
-        'src': state.track.mp3
+        'src': state.tracks[0].mp3
       })
     ]),
+    h('ol', state.tracks.map(function listTrack(track) {
+      return h('li', {
+        'id': track.cuid
+      }, track.mp3);
+    })),
     h('form', {
       'ev-event': hg.sendSubmit(state.channels.addTrack)
     }, [
